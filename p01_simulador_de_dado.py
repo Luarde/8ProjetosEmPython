8846c04afce341a37a5f815f660a9953436865fa
Luarde <luarde15@gmail.com>
2020-08-25 15:57:23 -0300

    Simulador de dado
    Simulador de dado usando a tela do PySimpleGUI.

--- p01_simulador_de_dado.py
+++ p01_simulador_de_dado.py
@@ -1,26 +1,37 @@
 # Simulador de dado
 # Simular o uso de um dado, gerando o valor de 1 até 6
 import random
+import PySimpleGUI as sg
+
 
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = 'Você gostaria de gerar um novo valor para o dado? '
+        # Layout
+        self.layout = [
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('sim'), sg.Button('não')]
+        ]
 
     def Iniciar(self):
-        resposta = input(self.mensagem)
-
+         # Criar uma janela
+        self.janela = sg.Window('Simulador de dado', layout=self.layout)
+        # Ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()
+        # Fazer alguma coisa com esses valores
+            
         try:
-            if resposta == 'sim' or resposta == 's':
+            if self.eventos == 'sim' or self.eventos == 's':                 
                 self.GerarValorDoDado()
-            elif resposta == 'não' or resposta == 'n':
-                print('Agradecemos sua participação!')
+            elif self.eventos == 'não' or self.eventos == 'n':
+                    print('Agradecemos sua participação!')
             else: 
-                print('Favor digitar sim ou não')
+                    print('Favor digitar sim ou não')
         except:
-                print('Ocorreu um erro ao receber sua resposta.')
-      
+                    print('Ocorreu um erro ao receber sua resposta.')
+                        
     def GerarValorDoDado(self):      
         print(random.randint(self.valor_minimo,self.valor_maximo))
 
