67ebd018dac463a42eb3a9859b6c3561fe6a38c4
Luarde <luarde15@gmail.com>
2020-08-26 17:22:30 -0300

    Projetos 4 e 5

--- 04_decida_por_mim.py
+++ 04_decida_por_mim.py
@@ -1,6 +1,7 @@
 # Projeto - 5 Decida por Mim
 # Faça uma pergunta para o programa, e ele terá que te dar uma resposta.
-import random
+import random 
+import PySimpleGUI as sg
 
 
 class DecidaPorMim:
@@ -13,8 +14,23 @@ class DecidaPorMim:
         ]
 
     def Iniciar(self):
-        input('Faça sua pergunta: ')
-        print(random.choice(self.respostas))
+        #Layout
+        layout = [
+            [sg.Text('Faça sua pergunta: ')],
+            [sg.Input()],
+            [sg.Output(size=(50,10))],
+            [sg.Button('Decida Por Mim')]
+        ]
+        # Criar uma janela
+        self.janela = sg.Window('Decida Por Mim!',layout=layout)
+        while True:
+            # Ler os valores
+            self.eventos, self.valores = self.janela.Read()
+            # Fazer alguma coisa Com os valores
+            if self.eventos == 'Decida Por Mim':
+                print(random.choice(self.respostas))
+
+
 
 decida = DecidaPorMim()
 decida.Iniciar()
